3e7f0695fa83ae3994fbbc5afb5054fdeff263c2
Ian Korf <ifkorf@gmail.com>
2015-08-27 12:59:34 -0700

    reorganization
    Some stuff not working right still

--- veintrace.pl
+++ veintrace.pl
@@ -53,26 +53,27 @@ print STDERR "done\n";
 
 ## Find Origin ##
 my $s0 = 10; # 10 degrees per step for origin
-my $l0 = 15; # 20 pixel origin length
-my $g0 = 16; # 21 pixel image gutter
-my $origin = find_origin($IMAGE[$RES_0], $s0, $l0,  $g0);
+my $h0 = 15; # 15 pixel head length
+my $t0 =  5; # 5 pixel tail length
+my $g0 = 16; # 16 pixel image gutter
+my $origin = find_origin($IMAGE[$RES_0], $s0, $h0, $t0, $g0);
 
 ## Trace Main Vein ##
-my $a1 = 15; # 30 degree arc allowed
-my $s1 = 5; # 5 degrees per step
-my $l1 = 5; # 5 pixel unit length
+my $a1 = 20; # arc degrees
+my $s1 = 5;  # degrees per step
+my $h1 = 5; # head length
+my $t1 = 5; # tail length
 
-my $unit = $origin;
 my @primary;
+my $unit = $origin;
 my $count = 0;
 my %seen;
-while ($unit) {
-	
+#print STDERR "tracing primary vein";
+while (1) {
+#	print STDERR ".";
 	my $t = threshold($IMAGE[$RES_0], $unit, $a1, $s1);
-	my ($next, $score) = next_unit($IMAGE[$RES_0], $unit, $a1, $l1, $s1);
-	
-	print_unit($unit, $score, $t);
-	
+	my ($next, $score) = next_unit($IMAGE[$RES_0], $unit, $a1, $h1, $t1, $s1);
+		
 	if ($score < $t) {
 		my $s = "$next->[0],$next->[1]";
 		if ($seen{$s}) {
@@ -85,13 +86,10 @@ while ($unit) {
 		push @primary, $next;
 		$unit = $next;
 	} else {
-		$unit = 0;
+		last;
 	}	
 }
-
-# hitting endless loops on t5 and t8
-# getting some kind of stable flip-flop
-
+#print STDERR "\n";
 
 ###################
 # Testing Section #
@@ -100,20 +98,26 @@ while ($unit) {
 if ($TEST) {
 
 	# origin
-	print "origin: ";
-	print_unit($origin, 0, 0);
-	
 	my $i0 = $IMAGE[$RES_0]->copy();
-	my ($x0, $y0, $a0) = @$origin;
+	my ($x0, $y0, $a0, $h0, $t0) = @$origin;
 	$i0->circle(color => 'green', 'x'=>$x0, 'y'=>$y0, 'r'=>3);
-	my $points = points_on_line([$x0, $y0, $a0, $l0]);
+	my ($x1, $y1) = @{head($origin)};
+	my $points = points_on_line($x0, $y0, $x1, $y1);
 	foreach my $point (@$points) {
 		my ($x, $y) = @$point;
 		$i0->setpixel('x'=>$x, 'y'=>$y, color=>'red');
 	}		
-	$i0->write(file=>"origin.png") or die Imager->errstr;
+	$i0->write(file=>"origin.png") or die Imager->errstr;	
 		
-	# primary vein
+	# primary vein	
+	my $i1 = $IMAGE[$RES_0]->copy();
+	foreach my $unit (@primary) {
+		my ($x, $y) = @$unit;
+		$i1->setpixel('x'=>$x, 'y'=>$y, color=>'red');
+	}
+	$i1->write(file=>"primary.png") or die Imager->errstr;
+
+	# all paths on the primary vein	
 	for (my $i = 0; $i < @primary; $i++) {
 		my $img = $IMAGE[$RES_0]->copy();
 		plot_unit($img, $primary[$i]);
@@ -121,230 +125,71 @@ if ($TEST) {
 		my $name = "primary-$n$i.png";
 		$img->write(file=>"$name") or die Imager->errstr;
 	}
-	
-	my $i1 = $IMAGE[$RES_0]->copy();
-	
-	foreach my $unit (@primary) {
-		my ($x, $y) = @$unit;
-		$i1->setpixel('x'=>$x, 'y'=>$y, color=>'red');
-		#plot_unit($i1, $unit);
-	}
-	
-	#my $last = $primary[-1];
-	#$i1->setpixel('x'=>$last->[0], 'y'=>$last->[1], color=>'yellow');
-	#plot_unit($i1, $last);
-	
-	$i1->write(file=>"primary.png") or die Imager->errstr;
-	
-	exit;
+	die "testing primary";
 }
 
 #############################################################################
 # Functions
 #############################################################################
 
-sub plot_unit {
-	my ($img, $unit) = @_;
-	my $points = points_on_line($unit);
-		
-	foreach my $point (@$points) {
-		my ($x, $y) = @$point;
-	#	print "$x,$y\n";
-		$img->setpixel('x'=>$x, 'y'=>$y, color=>'green');
-	}
-}
-
 sub angle {
 	my ($x0, $y0, $x1, $y1) = @_;
 	my $dx = $x1 - $x0;
 	my $dy = $y1 - $y0;
 	my $angle = atan2($dy, $dx);
-	return 360 * $angle / (2 * 3.1415); # -180 to 180
+	my $degrees = 360 * $angle / (2 * 3.1415); # -180 to 180
+	return int($degrees + 0.5);
 }
 
-sub end_point {
+sub head {
 	my ($unit) = @_;
-	my ($x0, $y0, $a0, $len) = @$unit;
+	my ($x0, $y0, $a0, $h0, $t0) = @$unit;
 	my $rad = 2 * 3.14159 * $a0 / 360;
-	my $x1 = $x0 + cos($rad) * $len;
-	my $y1 = $y0 + sin($rad) * $len;
+	my $x1 = $x0 + cos($rad) * $h0;
+	my $y1 = $y0 + sin($rad) * $h0;
+	$x1 = int($x1 + 0.5);
+	$y1 = int($y1 + 0.5);	
 	return [$x1, $y1];
 }
 
-sub threshold {
-	return 200;
-
-
-	my ($img, $unit, $arc, $step) = @_;
-	my ($x0, $y0, $a0, $len) = @$unit;
-	my %point;
-	for (my $angle = $a0 - $arc; $angle <= $a0 + $arc; $angle += $step) {
-		my $points = points_on_line([$x0, $y0, $angle, $len]);
-		foreach my $point (@$points) {
-			my $coor = "$point->[0],$point->[1]";
-			$point{$coor}++;
-		}
-	}
-	
-	my @val;
-	foreach my $coor (keys %point) {
-		my ($x, $y) = split(/,/, $coor);
-		my ($val) = $img->getpixel('x' => $x, 'y' => $y)->rgba;
-		push @val, $val;
-	}
-	@val = sort {$a <=> $b} @val;
-	my $t = $val[@val / 2]; # to be reconsidered
-	
-	return $t;
+sub tail {
+	my ($unit) = @_;
+	my ($x0, $y0, $a0, $h0, $t0) = @$unit;
+	$a0 += 180;
+	my $rad = 2 * 3.14159 * $a0 / 360;
+	my $x1 = $x0 + cos($rad) * $t0;
+	my $y1 = $y0 + sin($rad) * $t0;
+	$x1 = int($x1 + 0.5);
+	$y1 = int($y1 + 0.5);	
+	return [$x1, $y1];
 }
 
-sub print_unit {
-	my ($unit, $s, $t) = @_;
-#	printf "%d,%d %d° %d %d %d\n", @$unit, $s, $t;
-	print "@$unit $s $t\n";
+sub points_in_head {
+	my ($unit) = @_;
+	my ($x0, $y0) = @$unit;
+	my $head = head($unit);
+	my ($x1, $y1) = @$head;
+	my $points = points_on_line($x0, $y0, $x1, $y1, '-ori');
 }
 
-sub next_unit {
-	my ($img, $ori, $arc, $length, $step) = @_;
-
-	my ($x0, $y0, $a0) = @$ori;
-	my $aMin = $a0 - $arc;
-	my $aMax = $a0 + $arc;
-	
-	# find best line from here
-	my $min_score = 1e30;
-	my $best_unit;
-	for (my $angle = $aMin; $angle <= $aMax; $angle += $step) {
-		my $unit = [$x0, $y0, $angle, $length];
-		my $points = points_on_line($unit);
-		my $score = score_ave_points($img, $points);
-		if ($score < $min_score) {
-			$min_score = $score;
-			my $new_start = bestest_point($ori, $points);
-			$best_unit = [@$new_start, $angle , $length];			
-		}
-	}
-		
-	return $best_unit, $min_score;
+sub points_in_tail {
+	my ($unit) = @_;
+	my ($x0, $y0) = @$unit;
+	my $tail = tail($unit);
+	my ($x1, $y1) = @$tail;
+	my $points = points_on_line($x0, $y0, $x1, $y1, '-ori');
 }
 
-sub bestest_point {
-	my ($u1, $units) = @_;
-	my ($x0, $y0, $a0) = @$u1;
-
-	foreach my $unit (@$units) {
-		my ($x1, $y1) = @$unit;
-		next if $x1 == $x0 and $y1 == $y0;
-		next if abs($x0 - $x1) > 1 or abs($y0 - $y1) > 1;
-		return [$x1, $y1];
-	}
-		
-	die "unexpected";
-}
-
-sub find_origin {
-	my ($img, $step, $length, $gutter) = @_;
-	
-	my $ORIGINS = 100; # number of origins to consider
-
-	my $xlimit = $img->getwidth();
-	my $ylimit = $img->getheight();
-	print STDERR "finding origins in ($xlimit x $ylimit)\n";
-	
-	# look for darkest blobs
-	my @blob;
-	for (my $x = $gutter; $x < $xlimit -$gutter; $x++) {
-		for (my $y = $gutter; $y < $ylimit - $gutter; $y++) {
-			my $score = _score_blob($img, $x, $y);
-			push @blob, {
-				point => [$x, $y],
-				score => $score,
-			}
-		}
-		@blob = sort {$a->{score} <=> $b->{score}} @blob;
-		splice(@blob, $ORIGINS);
-	}
-			
-	# identify origin as dark line & light line opposite
-	# pointing towards the middle
-	my ($cx, $cy) = ($xlimit/2, $ylimit/2); # center of image
-	my $origin;
-	my $min_score = 1e30;
-	foreach my $item (@blob) {
-		my ($x, $y) = @{$item->{point}};
-		my $ta = angle($x, $y, $cx, $cy);
-		my $ca = $ta + 180;
-		my $a1 = $ta - 45;
-		my $a2 = $ta + 45;
-		
-		for (my $angle = $a1; $angle < $a2; $angle += $step) {
-			
-			# black vein
-			my $u1 = [$x, $y, $angle, $length];
-			my $p1 = points_on_line($u1);
-			my $s1 = score_ave_points($img, $p1);
-			
-			# white non-vein, shorter
-			my $u2 = [$x, $y, $angle + 180, $length/4];	
-			my $p2 = points_on_line($u2);
-			my $s2 = score_ave_points($img, $p2);
-			
-			my $score = $s1 - $s2;
-			if ($score < $min_score) {
-				$origin = [$x, $y, $angle, $length];
-				$min_score = $score;
-			}
-		}
-	}
-		
-	return $origin;
-}
-
-sub score_ave_points {
-	my ($img, $points) = @_;
-	
-	my $sum = 0;
-	foreach my $point (@$points) {
-		my ($x, $y) = @$point;
-		my ($val) = $img->getpixel('x' => $x, 'y' => $y)->rgba;
-		$sum += $val
-	}
-	
-	return int $sum / @$points;
-}
-
-sub score_sum_points {
-	my ($img, $points) = @_;
-	
-	my $sum = 0;
-	foreach my $point (@$points) {
-		my ($x, $y) = @$point;
-		my ($val) = $img->getpixel('x' => $x, 'y' => $y)->rgba;
-		$sum += $val
-	}
-	
-	return $sum;
-}
-
-sub count_points {
-	my ($img, $points, $t) = @_;
-		
-	my $count = 0;
-	foreach my $point (@$points) {
-		my ($x, $y) = @$point;
-		my ($val) = $img->getpixel('x' => $x, 'y' => $y)->rgba;
-		$count++ if $val >= $t;
-	}
-	
-	return $count;
+sub points_in_unit {
+	my ($unit) = @_;
+	my $origin = [$unit->[0], $unit->[1]];
+	my $head_points = points_in_head($unit);
+	my $tail_points = points_in_tail($unit);
+	return [$origin, @$head_points, @$tail_points];
 }
 
 sub points_on_line {
-	my ($unit) = @_;
-	
-	my ($x0, $y0, $a0, $len) = @$unit;
-	my $end = end_point($unit);
-	my ($x1, $y1) = @$end;
+	my ($x0, $y0, $x1, $y1, $mode) = @_;
 	
 	# round off values
 	$x0 = int($x0 + 0.5);
@@ -388,6 +233,8 @@ sub points_on_line {
 		}
 	}
 	
+	delete $point{"$x0,$y0"} if defined $mode and $mode eq '-ori';
+		
 	# return as array of (x,y) pairs
 	my @point;
 	foreach my $point (keys %point) {
@@ -398,13 +245,179 @@ sub points_on_line {
 	return \@point;
 }
 
+sub next_unit {
+	my ($img, $ori, $arc, $hlen, $tlen, $step) = @_;
+
+	my ($x0, $y0, $a0) = @$ori;
+	my $aMin = $a0 - $arc;
+	my $aMax = $a0 + $arc;
+	
+	# find optimal angle from here
+	print "angle in $a0\n";
+	my $min_score = 1e30;
+	my $opt_angle;
+	for (my $angle = $aMin; $angle <= $aMax; $angle += $step) {
+		my $unit = [$x0, $y0, $angle, $hlen, $tlen];
+		my $score = score_unit($img, $unit);
+		print "\t$angle° $score\n";
+		if ($score < $min_score) {
+			$min_score = $score;
+			$opt_angle = $angle;
+		}
+	}
+	print "angle out $opt_angle, score $min_score\n";
+	
+	# find nearest pixel on line
+	my $best = [$x0, $y0, $opt_angle, $hlen, $tlen];
+	my $points = points_in_head($best);
+	my ($next_x, $next_y);
+	foreach my $point (@$points) {
+		my ($x1, $y1) = @$point;
+		next if $x1 == $x0 and $y1 == $y0;
+		next if abs($x0 - $x1) > 1 or abs($y0 - $y1) > 1;
+		$next_x = $x1;
+		$next_y = $y1;
+	}
+	
+	my $next_unit = [$next_x, $next_y, $opt_angle, $hlen, $tlen];
+	print_unit($next_unit);
+	
+	return $next_unit, $min_score;
+}
+
+sub threshold {
+	return 220; # temporary
+
+
+	my ($img, $unit, $arc, $step) = @_;
+	my ($x0, $y0, $a0, $len) = @$unit;
+	my %point;
+	for (my $angle = $a0 - $arc; $angle <= $a0 + $arc; $angle += $step) {
+		my $points = points_on_line([$x0, $y0, $angle, $len]);
+		foreach my $point (@$points) {
+			my $coor = "$point->[0],$point->[1]";
+			$point{$coor}++;
+		}
+	}
+	
+	my @val;
+	foreach my $coor (keys %point) {
+		my ($x, $y) = split(/,/, $coor);
+		my ($val) = $img->getpixel('x' => $x, 'y' => $y)->rgba;
+		push @val, $val;
+	}
+	@val = sort {$a <=> $b} @val;
+	my $t = $val[@val / 2]; # to be reconsidered
+	
+	return $t;
+}
+
+
+sub find_origin {
+	my ($img, $step, $hlen, $tlen, $gutter) = @_;
+	
+	my $ORIGINS = 100; # number of origins to consider
+
+	my $xlimit = $img->getwidth();
+	my $ylimit = $img->getheight();
+	print STDERR "finding origins in ($xlimit x $ylimit)\n";
+	
+	# look for darkest blobs
+	my @blob;
+	for (my $x = $gutter; $x < $xlimit -$gutter; $x++) {
+		for (my $y = $gutter; $y < $ylimit - $gutter; $y++) {
+			my $score = _score_blob($img, $x, $y);
+			push @blob, {
+				point => [$x, $y],
+				score => $score,
+			}
+		}
+		@blob = sort {$a->{score} <=> $b->{score}} @blob;
+		splice(@blob, $ORIGINS);
+	}
+			
+	# identify origin as dark line & light line opposite
+	# with a path leading to the long side of the image
+	my ($cx, $cy) = ($xlimit/2, $ylimit/2); # center of image
+	my $origin;
+	my $min_score = 1e30;
+	foreach my $item (@blob) {
+		my ($x0, $y0) = @{$item->{point}};
+		my $ta = angle($x0, $y0, $cx, $cy);
+		my $ca = $ta + 180;
+		my $a1 = $ta - 45;
+		my $a2 = $ta + 45;
+		
+		for (my $angle = $a1; $angle < $a2; $angle += $step) {
+			my $unit = [$x0, $y0, $angle, $hlen, $tlen];
+			my $head = head($unit);
+			my $tail = tail($unit);
+			my $p1 = points_on_line($x0, $y0, @$head);
+			my $p2 = points_on_line($x0, $y0, @$tail, '-ori');
+			my $score;
+			foreach my $p (@$p1) {
+				my ($x, $y) = @$p;
+				my ($val) = $img->getpixel('x' => $x, 'y' => $y)->rgba;
+				$score += $val;
+			}
+			foreach my $p (@$p2) {
+				my ($x, $y) = @$p;
+				my ($val) = $img->getpixel('x' => $x, 'y' => $y)->rgba;
+				$score -= $val;
+			}
+			if ($score < $min_score) {
+				$origin = [$x0, $y0, $angle, $hlen, $tlen];
+				$min_score = $score;
+			}
+		}
+	}
+		
+	return $origin;
+}
+
+sub score_unit {
+	my ($img, $unit) = @_;
+	
+	my $sum = 0;
+	my $points = points_in_unit($unit);
+#	my @sum;
+	foreach my $point (@$points) {
+		my ($x, $y) = @$point;
+		my ($val) = $img->getpixel('x' => $x, 'y' => $y)->rgba;
+		$sum += $val;
+#		push @sum, $val;
+	}
+#	print "\t@sum\n";
+	
+	return int $sum / @$points;
+}
+
+sub plot_unit {
+	my ($img, $unit) = @_;
+	my $hp = points_in_head($unit);
+	foreach my $point (@$hp) {
+		my ($x, $y) = @$point;
+		$img->setpixel('x'=>$x, 'y'=>$y, color=>'green');
+	}
+	my $tp = points_in_tail($unit);
+	foreach my $point (@$tp) {
+		my ($x, $y) = @$point;
+		$img->setpixel('x'=>$x, 'y'=>$y, color=>'red');
+	}
+	$img->setpixel('x'=>$unit->[0], 'y'=>$unit->[1], color=>'blue');
+}
+
+sub print_unit {
+	my ($unit) = @_;
+	printf "%d,%d %d° %d %d\n", @$unit;
+}
 
 ########################
 # Single-use Functions #
 ########################
 
 sub _score_blob {
-	my ($img, $x, $y) = @_;
+	my ($img, $x0, $y0) = @_;
 		
 	my @blob = (
 		[ 0, -2],
@@ -422,14 +435,15 @@ sub _score_blob {
 		[ 0,  2],
 	);
 	
-	my @point;
+	my $score = 0;
 	foreach my $point (@blob) {
-		my $ix = $x + $point->[0];
-		my $iy = $y + $point->[1];
-		push @point, [$ix, $iy];
+		my $ix = $x0 + $point->[0];
+		my $iy = $y0 + $point->[1];
+		my ($val) = $img->getpixel('x' => $ix, 'y' => $iy)->rgba;
+		$score += $val;
 	}
 	
-	return score_sum_points($img, \@point);
+	return $score;
 }
 
 sub _perps {
@@ -452,11 +466,11 @@ A $point is a 2-part structure x,y coordinate pair held as an array
 reference. A @point is considered an unordered collection. $points is
 the preferred name for a reference to @point.
 
-A $unit is a 4-part structure that contains x, y, angle, and length. The
-x,y point is considered the MIDDLE of the unit with a head and tail
-extending in opposite directions. The angle is the direction of the
-HEAD. A @unit is considered an unorderd collection and $units is the
-preferred name for a reference of @unit.
+A $unit is a 5-part structure that contains x, y, angle, head-length,
+and tail-length. The x,y point is considered the MIDDLE of the unit with
+a head and tail extending in opposite directions. The angle is the
+direction of the HEAD. A @unit is considered an unordered collection and
+$units is the preferred name for a reference of @unit.
 
 A $surface' is a collection of $unit stored in a 2-dimensional hash
 reference in which the x and y coordinates are the hash keys. This
